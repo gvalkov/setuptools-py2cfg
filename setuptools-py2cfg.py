618150e8ef655e41479836535d44218e1218f812
Georgi Valkov <georgi.t.valkov@gmail.com>
2018-04-30 22:04:27 +0200

    Remove trailing whitespace from resulting ini file

--- setuptools-py2cfg.py
+++ setuptools-py2cfg.py
@@ -73,7 +73,10 @@ def main(args=None):
     config.write(buf)
 
     # Convert leading tabs to spaces.
-    return re.sub('^(\t+)', ' ' * args.dangling_list_indent, buf.getvalue(), 0, re.MULTILINE)
+    res = re.sub('^(\t+)', ' ' * args.dangling_list_indent, buf.getvalue(), 0, re.MULTILINE)
+
+    # Remove trailing whitespace.
+    return re.sub(' +$', '', res, 0, re.MULTILINE)
 
 
 def py2cfg(setup, setuppy_dir, dangling_list_threshold):
